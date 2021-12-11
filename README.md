e7c58791e2d90e0e6a525d628cb42d0e802a286a
theemadnes <alex.mattson@gmail.com>
2021-12-11 01:31:56 -0600

    first

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # microservices-saga-sample
 Naive attempt at using multiple microservices that implement the saga pattern with idempotency.
+
+### setup
+
+create frontend topic
+
+```
+gcloud pubsub topics create frontend
+```
---
+++ frontend/app.py
@@ -0,0 +1,36 @@
+import json
+import uuid
+import os
+from dotenv import load_dotenv
+from google.cloud import pubsub_v1
+from flask import Flask, request, Response, jsonify
+
+load_dotenv()
+app = Flask(__name__)
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(os.environ.get('PROJECT_ID'), os.environ.get('TOPIC'))
+
+
+@app.route('/')
+def index():
+    return 'Index Page\n'
+
+@app.route('/order', methods=['GET', 'POST'])
+def order():
+    if request.method == 'POST':
+        # add UUID to request JSON
+        request.json['uuid'] = str(uuid.uuid4())
+        data = json.dumps(request.json).encode("UTF-8")
+        print("Publishing " + json.dumps(request.json))
+        future = publisher.publish(topic_path, data)
+        #return 'called POST\n'
+        print(future.result())
+        return f"Published messages to {topic_path}." + "\n"
+    else:
+        return 'called GET\n'
+
+if __name__ == '__main__':
+
+    app.run(
+            host='0.0.0.0', port=int(os.environ.get('PORT', 8080)),
+            threaded=True)
